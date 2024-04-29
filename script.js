b1ac84a3532282799a47b7fc8dd883f0e6fda3aa
NehalH <nhosalikar@gmail.com>
2024-04-29 16:56:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var gateway = `ws://192.168.4.1/ws`;
+var websocket;
+
+window.addEventListener('load', onload);
+
+function onload(event) {
+    initWebSocket();
+}
+
+function initWebSocket() {
+    console.log('Trying to open a WebSocket connection…');
+    websocket = new WebSocket(gateway);
+    websocket.onopen = onOpen;
+    websocket.onclose = onClose;
+    websocket.onmessage = onMessage;
+}
+
+function onOpen(event) {
+    console.log('Connection opened');
+}
+
+function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+}
+
+function onMessage(event) {
+    console.log(event.data);
+    var blob = new Blob([event.data], { type: 'image/jpeg' });
+    var url = URL.createObjectURL(blob);
+    document.getElementById("camera-image").src = url;
+}
